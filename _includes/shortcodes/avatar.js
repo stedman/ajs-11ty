2ef40f2ccd1ae1d92129ba0bd31eae4a4f44ff01
Steve Stedman <steve@stedman.dev>
2020-05-27 13:24:31 -0500

    Update to live site code, version avatar output

---
+++ _includes/shortcodes/avatar.js
@@ -0,0 +1,28 @@
+const Image = require('@11ty/eleventy-img');
+
+module.exports = async (src, alt, className, outputFormat = 'jpeg') => {
+  if (alt === undefined) {
+    throw new Error(`Missing ALT attribute from: ${src}`);
+  }
+
+  const classAttr = className ? `class="${className}"` : '';
+
+  try {
+    const stats = await Image(src, {
+      cacheDuration: '12w',
+      formats: [outputFormat],
+      urlPath: '/ajs-11ty/assets/avatar/', // path prefix: /ajs-11ty/
+      outputDir: 'assets/avatar/',
+      widths: [96],
+    });
+
+    const props = stats[outputFormat].pop();
+
+    return `<img ${classAttr} src="${props.url}" width="${props.width}" height="${props.height}" alt="${alt}" loading="lazy">`;
+  } catch (err) {
+    // eslint-disable-next-line no-console
+    console.error(err, `alt: ${alt}`);
+
+    return '';
+  }
+};
--- _includes/shortcodes/meetup-details.js
+++ _includes/shortcodes/meetup-details.js
@@ -1,8 +1,24 @@
 const organizations = require('../../_data/organizations.json');
 const siteData = require('../../_data/site.json');
-const filterFullDate = require('../filters/full-date');
 
 const pathPrefix = '/ajs-11ty';
+
+/**
+ * Filter date output to follow pattern MMMM D, YYYY
+ *
+ * @param  {object}   dateValue  Date object
+ *
+ * @return {string}   Formatted date string.
+ */
+const fullDate = (dateValue) => {
+  const monthNames = [
+    'January', 'February', 'March', 'April', 'May', 'June',
+    'July', 'August', 'September', 'October', 'November', 'December',
+  ];
+  const newDate = new Date(dateValue);
+
+  return `${monthNames[newDate.getMonth()]} ${newDate.getDate()}, ${newDate.getFullYear()}`;
+};
 
 /**
  * Provide template for meeting details.
@@ -60,7 +76,7 @@ module.exports = function meetupDetails(meetDate, venue, after, msgHeader, meetT
       <span class="icon has-text-grey-dark">
         <ion-icon src="${pathPrefix}/assets/ionicon/calendar.svg"></ion-icon>
       </span>
-      <time class="has-text-primary has-text-weight-bold" dateTime="${meetDate.toISOString().substring(0, 10)}">${filterFullDate(meetDate)}</time>
+      <time class="has-text-primary has-text-weight-bold" dateTime="${meetDate.toISOString().substring(0, 10)}">${fullDate(meetDate)}</time>
     </div>
 
     <div>
